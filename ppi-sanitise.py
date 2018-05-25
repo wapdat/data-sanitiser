ea5ac818ece73dfb2e84dcd6fdbe5b854c89d8b8
Lindsay Smith <wapdat@users.noreply.github.com>
2018-05-25 20:14:28 +0100

    Update ppi-sanitise.py

--- ppi-sanitise.py
+++ ppi-sanitise.py
@@ -2,34 +2,45 @@ from __future__ import print_function
 
 import sys
 import re
+import nltk
 
 if len(sys.argv) < 2:
     print('Exit code 10: Please enter a string to sanitise')
     sys.exit(10)
     
-dirtyStr = sys.argv[1]
+str = sys.argv[1]
 
-print('%-30s "%s"' % ('Unsanitised', dirtyStr))
+def sanitise( str, desc, token, regex):
+    r = re.compile(regex)
+    cleanStr = re.sub( r, token, str, re.I)
+    print('%-30s "%s"' % (desc, cleanStr))
+    return cleanStr;
 
-cleanStr = re.sub( r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', 'NAME@EMAIL.COM', dirtyStr, re.I)
-print('%-30s "%s"' % ('cleaned emails', cleanStr))
+print('%-30s "%s"' % ('Unsanitised', str))
 
-cleanStr = re.sub( r'(gir ?0aa|GIR ?0AA|[a-pr-uwyzA-PR-UWYZ]([0-9]{1,2}|([a-hk-yA-HK-Y][0-9]([0-9abehmnprv-yABEHMNPRV-Y])?)|[0-9][a-hjkps-uwA-HJKPS-UW]) ?[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2})', 'POSTCODE', cleanStr, re.IGNORECASE)
-print('%-30s "%s"' % ('cleaned UK postcodes', cleanStr))
-
-cleanStr = re.sub( r'(?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}', 'SSN', cleanStr, re.IGNORECASE)
-print('%-30s "%s"' % ('cleaned dashed SSN', cleanStr))
-
-cleanStr = re.sub( r'(\s|^)(?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}(\s|$)', 'SSN', cleanStr, re.IGNORECASE)
-print('%-30s "%s"' % ('cleaned SSN', cleanStr))
-
-cleanStr = re.sub( r'(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?', 'UKPHONE', cleanStr, re.IGNORECASE)
-print('%-30s "%s"' % ('cleaned UK Phone', cleanStr))
+str = sanitise(str, 'NAME@EMAIL.COM', 'email address', '[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+str = sanitise(str, 'UKPOSTCODE', 'uk postcode', '(gir ?0aa|GIR ?0AA|[a-pr-uwyzA-PR-UWYZ]([0-9]{1,2}|([a-hk-yA-HK-Y][0-9]([0-9abehmnprv-yABEHMNPRV-Y])?)|[0-9][a-hjkps-uwA-HJKPS-UW]) ?[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2})')
+str = sanitise(str, 'SSN', 'ssn', '(?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}')
+str = sanitise(str, 'SSN', 'ssn', '(\s|^)(?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}(\s|$)')
+str = sanitise(str, 'UKPHONE', 'uk phone', '(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?')
 
 # Problem with chomping leading and training space
-cleanStr = re.sub( r'(1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?)', ' USPHONE ', cleanStr, re.IGNORECASE)
-print('%-30s "%s"' % ('cleaned US Phone', cleanStr))
-
-cleanStr = re.sub( r'(\s|^)(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?(\s|$)', 'USPHONE', cleanStr, re.IGNORECASE)
-print('%-30s "%s"' % ('cleaned US Phone', cleanStr))
+str = sanitise(str, 'CARDNUM', ' USPHONE ', '(1?\W*([2-9][0-8][0-9])\W*([2-9][0-9]{2})\W*([0-9]{4})(\se?x?t?(\d*))?)')
+str = sanitise(str, 'CARDNUM', 'USPHONE', '(\s|^)(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?(\s|$)')
+str = sanitise(str, 'CARDNUM', 'Amex', '3[47][0-9]{13}')
+str = sanitise(str, 'CARDNUM', 'BCGlobal', '(6541|6556)[0-9]{12}')
+str = sanitise(str, 'CARDNUM', 'Carte Blanche Card', '389[0-9]{11}')
+str = sanitise(str, 'CARDNUM', 'Diners Club Card', '3(?:0[0-5]|[68][0-9])[0-9]{11}')
+str = sanitise(str, 'CARDNUM', 'Discover Card', '65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})')
+str = sanitise(str, 'CARDNUM', 'Insta Payment Card', '63[7-9][0-9]{13}')
+str = sanitise(str, 'CARDNUM', 'JCB Card', '(?:2131|1800|35\d{3})\d{11}$')
+str = sanitise(str, 'CARDNUM', 'KoreanLocalCard', '9[0-9]{15}')
+str = sanitise(str, 'CARDNUM', 'Laser Card', '(6304|6706|6709|6771)[0-9]{12,15}')
+str = sanitise(str, 'CARDNUM', 'Maestro Card', '(5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}')
+str = sanitise(str, 'CARDNUM', 'Mastercard', '5[1-5][0-9]{14}')
+str = sanitise(str, 'CARDNUM', 'Solo Card', '(6334|6767)[0-9]{12}|(6334|6767)[0-9]{14}|(6334|6767)[0-9]{15}')
+str = sanitise(str, 'CARDNUM', 'Switch Card', '(4903|4905|4911|4936|6333|6759)[0-9]{12}|(4903|4905|4911|4936|6333|6759)[0-9]{14}|(4903|4905|4911|4936|6333|6759)[0-9]{15}|564182[0-9]{10}|564182[0-9]{12}|564182[0-9]{13}|633110[0-9]{10}|633110[0-9]{12}|633110[0-9]{13}')
+str = sanitise(str, 'CARDNUM', 'Union Pay Card', '(62[0-9]{14,17})')
+str = sanitise(str, 'CARDNUM', 'Visa Card', '4[0-9]{12}(?:[0-9]{3})?')
+str = sanitise(str, 'CARDNUM', 'Visa Master Card', '(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})')
 
